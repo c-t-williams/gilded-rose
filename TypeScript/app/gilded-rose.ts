ad96bdcc11dc38fb9b4731f4e818ae669d694626
christopher williams <christopher.williams@progressivecontent.com>
2020-11-07 11:30:31 +0000

    Adding escape hatch for legendary items, as they do not change

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -19,14 +19,16 @@ export class GildedRose {
 
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
+
+            // Escape hatch, ignoring Legendary Items
+            if (this.items[i].name === 'Sulfuras, Hand of Ragnaros') continue;
+
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        if (this.items[i].name != 'Conjured Mana Cake') {
-                            this.items[i].quality = this.items[i].quality - 1
-                        } else {
-                            this.items[i].quality = this.items[i].quality - 2
-                        }
+                    if (this.items[i].name != 'Conjured Mana Cake') {
+                        this.items[i].quality = this.items[i].quality - 1
+                    } else {
+                        this.items[i].quality = this.items[i].quality - 2
                     }
                 }
             } else {
@@ -46,19 +48,17 @@ export class GildedRose {
                     }
                 }
             }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
+
+            this.items[i].sellIn = this.items[i].sellIn - 1;
+
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Aged Brie') {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                if (this.items[i].name != 'Conjured Mana Cake') {
-                                    this.items[i].quality = this.items[i].quality - 1
-                                } else {
-                                    this.items[i].quality = this.items[i].quality - 2
-                                }
+                            if (this.items[i].name != 'Conjured Mana Cake') {
+                                this.items[i].quality = this.items[i].quality - 1
+                            } else {
+                                this.items[i].quality = this.items[i].quality - 2
                             }
                         }
                     } else {
